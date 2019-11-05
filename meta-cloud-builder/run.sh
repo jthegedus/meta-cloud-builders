00cc1cd850114d0e4e308ad76a7eb223cf0d6cf6
jthegedus <james@jthegedus.com>
2019-11-05 15:52:06 +1100

    fix parallel

--- meta-cloud-builder/run.sh
+++ meta-cloud-builder/run.sh
@@ -30,19 +30,21 @@ printf "[info] loading from config file: %s\n" "$config_file"
 
 # for each repo, clone to workspace
 jq -r '.[] | .repo' "$config_file" | 
-    while read -r url; do {
-        printf "[info] cloning repo %s\n" "$url"
-        final_path="${url//https:\/\/github.com\//}"
-        git clone "$url" "$workspace_dir/$final_path"
+    while read -r url; do
+        (
+            printf "[info] cloning repo %s\n" "$url"
+            final_path="${url//https:\/\/github.com\//}"
+            git clone "$url" "$workspace_dir/$final_path"
 
-        # for each builder in the config for this repo, build with gcloud
-        jq -r --arg repository "$url" '.[] | select(.repo == $repository) | .builders | .[]' "$config_file" |
-            while read -r builder; do {
+            # for each builder in the config for this repo, build with gcloud
+            jq -r --arg repository "$url" '.[] | select(.repo == $repository) | .builders | .[]' "$config_file" |
+            while read -r builder; do
                 printf "\n[info] building %s\n" "$builder"
                 gcloud builds submit \
                     --timeout=900s \
                     --config "$workspace_dir/$final_path/$builder/cloudbuild.yaml" \
-                    "$workspace_dir/$final_path/$builder" &
-            } done
-    } done
+                    "$workspace_dir/$final_path/$builder"
+            done
+        ) &
+    done
 wait
