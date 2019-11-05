1a4a148139c8e0b4dcf8aa9531b8d86b6017492e
jthegedus <james@jthegedus.com>
2019-11-05 16:18:41 +1100

    fix parallel builds

--- meta-cloud-builder/run.sh
+++ meta-cloud-builder/run.sh
@@ -28,23 +28,40 @@ fi
 
 printf "[info] loading from config file: %s\n" "$config_file"
 
+failure_file=failure.log
+touch ${failure_file}
+
 # for each repo, clone to workspace
 jq -r '.[] | .repo' "$config_file" | 
     while read -r url; do
-        (
-            printf "[info] cloning repo %s\n" "$url"
-            final_path="${url//https:\/\/github.com\//}"
-            git clone "$url" "$workspace_dir/$final_path"
+        printf "[info] cloning repo %s\n" "$url"
+        final_path="${url//https:\/\/github.com\//}"
+        git clone "$url" "$workspace_dir/$final_path"
 
-            # for each builder in the config for this repo, build with gcloud
-            jq -r --arg repository "$url" '.[] | select(.repo == $repository) | .builders | .[]' "$config_file" |
-            while read -r builder; do
+        # for each builder in the config for this repo, build with gcloud
+        jq -r --arg repository "$url" '.[] | select(.repo == $repository) | .builders | .[]' "$config_file" |
+        while read -r builder; do
+            (
                 printf "\n[info] building %s\n" "$builder"
                 gcloud builds submit \
                     --timeout=900s \
                     --config "$workspace_dir/$final_path/$builder/cloudbuild.yaml" \
-                    "$workspace_dir/$final_path/$builder"
-            done
-        ) &
+                    "$workspace_dir/$final_path/$builder" > "$builder.log" 2>&1
+                if [[ $? -ne 0 ]]; then
+                    echo "$builder failed" | tee -a ${failure_file}
+                    cat "$builder.log"
+                fi
+            ) &
+        done
     done
 wait
+
+
+# Check if there is any failure.
+if [[ -s ${failure_file} ]]; then
+    echo
+    echo "Some builds failed:"
+    cat ${failure_file}
+    echo "Exiting."
+    exit 1
+fi
