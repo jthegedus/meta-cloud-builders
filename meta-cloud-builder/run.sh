ad58843a26df6a9e9c497bf6e20057744f162775
James Hegedus <james@jthegedus.com>
2020-06-02 19:17:15 +1000

    fix: format everything

--- meta-cloud-builder/run.sh
+++ meta-cloud-builder/run.sh
@@ -4,41 +4,41 @@ config_file=""
 workspace_dir="/workspace/builder_repos"
 
 if [ ! -f "$1" ]; then
-    printf "[error] first arg should be a config file. File %s does NOT exist\n" "$1"
-    exit 1
+	printf "[error] first arg should be a config file. File %s does NOT exist\n" "$1"
+	exit 1
 fi
 
 if [[ "$1" == *.yaml ]]; then
-    printf "[info] converting .yaml file to .json\n"
-    yq r -j "$1" > "${1%.yaml}.json"
-    config_file="${1%.yaml}.json"
+	printf "[info] converting .yaml file to .json\n"
+	yq r -j "$1" >"${1%.yaml}.json"
+	config_file="${1%.yaml}.json"
 else
-    config_file="$1"
+	config_file="$1"
 fi
 
 if [[ "$config_file" != *.json ]]; then
-    printf "[error] invalid config file %s File must be .json or .yaml\n" "$config_file"
-    exit 1
+	printf "[error] invalid config file %s File must be .json or .yaml\n" "$config_file"
+	exit 1
 fi
 
 printf "[info] loading from config file: %s\n" "$config_file"
 
 # for each repo, clone to workspace
-jq -r '.[] | .repo' "$config_file" | 
-    while read -r url; do
-        printf "[info] cloning repo %s\n" "$url"
-        final_path="${url//https:\/\/github.com\//}"
-        git clone "$url" "$workspace_dir/$final_path"
+jq -r '.[] | .repo' "$config_file" |
+	while read -r url; do
+		printf "[info] cloning repo %s\n" "$url"
+		final_path="${url//https:\/\/github.com\//}"
+		git clone "$url" "$workspace_dir/$final_path"
 
-        # for each builder in the config for this repo, build with gcloud
-        jq -r --arg repository "$url" '.[] | select(.repo == $repository) | .builders | .[]' "$config_file" |
-        while read -r builder; do
-            printf "\n[info] building %s\n" "$builder"
-            set -x
-            gcloud builds submit \
-                "${@:2}" \
-                --config "$workspace_dir/$final_path/$builder/cloudbuild.yaml" \
-                "$workspace_dir/$final_path/$builder"
-            set +x
-        done
-    done
+		# for each builder in the config for this repo, build with gcloud
+		jq -r --arg repository "$url" '.[] | select(.repo == $repository) | .builders | .[]' "$config_file" |
+			while read -r builder; do
+				printf "\n[info] building %s\n" "$builder"
+				set -x
+				gcloud builds submit \
+					"${@:2}" \
+					--config "$workspace_dir/$final_path/$builder/cloudbuild.yaml" \
+					"$workspace_dir/$final_path/$builder"
+				set +x
+			done
+	done
--- meta-triggers/run.sh
+++ meta-triggers/run.sh
@@ -7,80 +7,80 @@
 
 dir=${1:-"."}
 suffix=${2:-".*\.trigger\.(json|yaml)"}
-workspace="/workspace/meta_triggers_tmp"
+tmp_workspace="/workspace/meta_triggers_tmp"
 
 function has_duplicates() {
-  {
-    sort | uniq -d | grep . -qc
-  } < "$1"
+	{
+		sort | uniq -d | grep . -qc
+	} <"$1"
 }
 
 function cleanup() {
-    rm -rf "$workspace"
+	rm -rf "$tmp_workspace"
 }
 
-mkdir -p "$workspace"
+mkdir -p "$tmp_workspace"
 
 printf "[info] dir:\t%s\n" "$dir"
 printf "[info] suffix:\t%s\n" "$suffix"
 
 # read trigger names into a file
-find "$dir" -type f | grep -E "$suffix" > "$workspace/trigger_configs.txt"
+find "$dir" -type f | grep -E "$suffix" >"$tmp_workspace/trigger_configs.txt"
 while read -r trigger_file; do
-    if [[ "$trigger_file" == *.json ]]; then
-        # json
-        jq -r '.name' "$trigger_file" >> "$workspace/config_trigger_names.txt"
-    else
-        # yaml
-        yq r "$trigger_file" 'name' >> "$workspace/config_trigger_names.txt"
-    fi
-done < "$workspace/trigger_configs.txt"
+	if [[ "$trigger_file" == *.json ]]; then
+		# json
+		jq -r '.name' "$trigger_file" >>"$tmp_workspace/config_trigger_names.txt"
+	else
+		# yaml
+		yq r "$trigger_file" 'name' >>"$tmp_workspace/config_trigger_names.txt"
+	fi
+done <"$tmp_workspace/trigger_configs.txt"
 
 # error on duplicate trigger names
-if has_duplicates "$workspace/config_trigger_names.txt"; then
-    triggers=$(cat "$workspace/config_trigger_names.txt")
-    printf "\n[error] You have duplicate _name_ fields across Trigger configs. Names must be unique. Check your Triggers:\n\n%s" "$triggers"
-    cleanup
-    exit 1
+if has_duplicates "$tmp_workspace/config_trigger_names.txt"; then
+	triggers=$(cat "$tmp_workspace/config_trigger_names.txt")
+	printf "\n[error] You have duplicate _name_ fields across Trigger configs. Names must be unique. Check your Triggers:\n\n%s" "$triggers"
+	cleanup
+	exit 1
 fi
 
-gcloud beta builds triggers list > "$workspace/existing_triggers.yaml" # TODO: use this file to diff each proposed trigger update with the existing config to actually only update the changed ones
-grep -E "^name:\s.*$" "$workspace/existing_triggers.yaml" | sed -e 's/name:\s//g' > "$workspace/existing_trigger_names.txt"
+gcloud beta builds triggers list >"$tmp_workspace/existing_triggers.yaml" # TODO: use this file to diff each proposed trigger update with the existing config to actually only update the changed ones
+grep -E "^name:\s.*$" "$tmp_workspace/existing_triggers.yaml" | sed -e 's/name:\s//g' >"$tmp_workspace/existing_trigger_names.txt"
 
 printf "\n[info] existing triggers\n"
-cat "$workspace/existing_trigger_names.txt"
+cat "$tmp_workspace/existing_trigger_names.txt"
 
 printf "\n[info] triggers in config files\n"
-cat "$workspace/config_trigger_names.txt"
+cat "$tmp_workspace/config_trigger_names.txt"
 
 printf "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
 printf "\n[info] triggers to CREATE\n" # config NOT IN existing
-comm -23 <(sort "$workspace/config_trigger_names.txt") <(sort "$workspace/existing_trigger_names.txt")
+comm -23 <(sort "$tmp_workspace/config_trigger_names.txt") <(sort "$tmp_workspace/existing_trigger_names.txt")
 printf "\n[info] triggers to UPDATE\n" # config IN existing
-comm -12 <(sort "$workspace/config_trigger_names.txt") <(sort "$workspace/existing_trigger_names.txt")
+comm -12 <(sort "$tmp_workspace/config_trigger_names.txt") <(sort "$tmp_workspace/existing_trigger_names.txt")
 printf "\n[info] triggers to DELETE\n" # existing NOT IN config
-comm -13 <(sort "$workspace/config_trigger_names.txt") <(sort "$workspace/existing_trigger_names.txt") | tee -a  "$workspace/triggers_to_delete.txt"
+comm -13 <(sort "$tmp_workspace/config_trigger_names.txt") <(sort "$tmp_workspace/existing_trigger_names.txt") | tee -a "$tmp_workspace/triggers_to_delete.txt"
 
 printf "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
 printf "\n[info] creating & updating triggers"
 while read -r line; do
-    set -x
-    gcloud beta builds triggers import --source="$line"
-    set +x
-done < "$workspace/trigger_configs.txt" # can use original trigger_configs.txt file since there are no duplicates identified and all config files are going to be imported regardless of a CREATE or UPDATE
+	set -x
+	gcloud beta builds triggers import --source="$line"
+	set +x
+done <"$tmp_workspace/trigger_configs.txt" # can use original trigger_configs.txt file since there are no duplicates identified and all config files are going to be imported regardless of a CREATE or UPDATE
 
 printf "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
 printf "\n[info] deleting triggers"
 while read -r trigger_name; do
-    yq r -d'*' -j "$workspace/existing_triggers.yaml" > "$workspace/existing_triggers.json"
-    trigger_id=$(jq -r --arg trigger_name "$trigger_name" '.[] | select(.name == $trigger_name) | .id' "$workspace/existing_triggers.json")
-    set -x
-    gcloud beta builds triggers delete --quiet "$trigger_id"
-    set +x
-done < "$workspace/triggers_to_delete.txt"
+	yq r -d'*' -j "$tmp_workspace/existing_triggers.yaml" >"$tmp_workspace/existing_triggers.json"
+	trigger_id=$(jq -r --arg trigger_name "$trigger_name" '.[] | select(.name == $trigger_name) | .id' "$tmp_workspace/existing_triggers.json")
+	set -x
+	gcloud beta builds triggers delete --quiet "$trigger_id"
+	set +x
+done <"$tmp_workspace/triggers_to_delete.txt"
 
 # fin
 cleanup
